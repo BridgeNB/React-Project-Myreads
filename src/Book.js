d1cddb7bd5c13cb1330e8b7a944ec9aa3d9ce422
BridgeNB <zyq123258789@gmail.com>
2018-01-20 17:08:33 -0800

    Myreads heroku added

--- src/Book.js
+++ src/Book.js
@@ -16,14 +16,12 @@ class Book extends Component {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{
-              width: 128,
-              height: 188,
               backgroundImage: `url(${coverImage})`
             }}></div>
           <ShelfSwitcher book={book} books={books} switchShelf={switchShelf}/>
         </div>
         <div className="book-title">{title}</div>
-        {book.authors && book.authors.map((author, index) => (<div className="book-authors" key={index}>{author}</div>))}
+        {book.authors ? book.authors.join(`,`): ``}
       </div>
     </li>)
   }
